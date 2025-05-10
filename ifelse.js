81065cdf75aff1b49b86ef70d531e0370c19a56f
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2025-05-10 17:15:59 -0300

    ifelse

---
+++ ifelse.js
@@ -0,0 +1,15 @@
+let numero = -5;
+
+if (numero > 0) {
+  console.log("O número é positivo.");
+} else {
+  console.log("O número é negativo.");
+}
+
+let numero2 = 8;
+
+if (numero2 % 2 === 0) {
+  console.log("O número é par.");
+} else {
+  console.log("O número é ímpar.");
+}
