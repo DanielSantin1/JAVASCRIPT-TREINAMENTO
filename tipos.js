f26e9a3047cae7b481b276153fdfd3c2e1ec23cd
Daniel Santin <86211080+DanielSantin1@users.noreply.github.com>
2025-05-09 21:14:45 -0300

    tipos, variaveis

---
+++ tipos.js
@@ -0,0 +1,17 @@
+let nome = "Carlos"; // String
+let idade = 30; // Number
+let estaTrabalhando = true; // Boolean
+let numeros = [1, 2, 3, 4]; // Array é objeto
+let pessoa = { nome: "Carlos", idade: 30 }; // Object é um objeto
+let nulo = null; // Null é um objeto
+let indefinido; // Undefined
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof estaTrabalhando);
+console.log(typeof numeros);
+console.log(typeof pessoa);
+console.log(typeof nulo); // null é um objeto
+console.log(typeof indefinido); // undefined é um tipo primitivo
+
+// TUDO É OBJETO
---
+++ váriavéis.js
@@ -0,0 +1,10 @@
+let nome = "Maria"; // Variável que pode ser alterada
+const idade = 25; // Constante que não pode ser alterada
+
+console.log(nome); // Imprime "Maria"
+console.log(idade); // Imprime 25
+
+nome = "João"; // Reatribui o valor da variável nome, ou seja, JavaScript permite que variáveis sejam reatribuídas, mas constantes não.
+
+console.log(nome); // Imprime "João"
+// console.log(idade); // Isso causaria um erro, pois idade é uma constante
